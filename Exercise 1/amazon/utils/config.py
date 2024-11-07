54d8fc233bdaebda922b9d5d29525df21af01a22
Lukas Sichert <lukas.sichert@gmail.com>
2024-11-07 19:30:36 +0100

    test

--- Exercise 1/amazon/utils/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#test
-class Config:
-    BASE_DIR = "/home/lukas/OneDrive/Dokumente/Uni/ML/Machine-Learning/Exercise 1/amazon"
-    DATA_DIR = os.path.join(BASE_DIR, "data")
-    LOG_DIR = os.path.join(BASE_DIR, "logs")
-    SUBMISSION_DIR = os.path.join(BASE_DIR, "submissions")
-    PLOTS_DIR = os.path.join(BASE_DIR, "plots")
