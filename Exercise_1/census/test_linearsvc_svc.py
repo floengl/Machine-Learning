f6bb3858ef5a385655fecb3440a8cc5ef61d9179
floengl <florian.engl@tuwien.ac.at>
2024-11-08 19:03:42 +0100

    linearsvc vs svc on census

--- Exercise_1/census/test_linearsvc_svc.py
+++ Exercise_1/census/test_linearsvc_svc.py
@@ -1,12 +1,14 @@
-from utils import load_training_dataset, setup_logging
+from utils import load_training_dataset, setup_logging, numeric, categorical
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import (OneHotEncoder, StandardScaler, MinMaxScaler, RobustScaler,
+                                   MaxAbsScaler, QuantileTransformer, PowerTransformer)
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import RepeatedStratifiedKFold, cross_validate
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 import numpy as np
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 # Set up logging
 logger = setup_logging("test_linearscv_svc")
@@ -14,15 +16,28 @@ logger = setup_logging("test_linearscv_svc")
 # load dataset
 X, y = load_training_dataset()
 
-# define preprocessing pipeline
-preprocessor = Pipeline([
-    ("remove_ID", ColumnTransformer([("remove_ID", "drop", "ID")], remainder="passthrough")),
-    ("max_abs_scaler", MaxAbsScaler())
+# categorical preprocessing: nan is its own category
+nan_is_category = Pipeline([
+    ("imputer", SimpleImputer(strategy="constant", fill_value="nan")),
+    ("one_hot_encoder", OneHotEncoder(drop="if_binary", handle_unknown="ignore"))
 ])
+
+# define preprocessing pipeline
+#preprocessor = Pipeline([
+#    ("remove_ID", ColumnTransformer([("remove_ID", "drop", "ID")], remainder="passthrough")),
+#    ("max_abs_scaler", MaxAbsScaler())
+#])
+
+# create preprocessor
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("numeric", MaxAbsScaler(), numeric),
+        ("categorical", nan_is_category, categorical)
+    ]
+)
 
 # models to test with
 models = [
-    #("RandomForest", RandomForestClassifier(random_state=1234)),
     ("LinearSVC", LinearSVC(random_state=1234)),
     ("SVC", SVC(random_state=1234,kernel="linear"))
 ]
@@ -32,7 +47,7 @@ for model_name, model in models:
     accuracy = []
     f1 = []
     cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=5, random_state=1234)
-    scores = cross_validate(Pipeline([("model",model)]), X, y, scoring=["accuracy", "f1_macro"], cv=cv, n_jobs=-1)
+    scores = cross_validate(Pipeline([("preprocessor",preprocessor),("model",model)]), X, y, scoring=["accuracy", "f1_macro"], cv=cv, n_jobs=-1)
     accuracy.append((np.mean(scores['test_accuracy']), np.std(scores['test_accuracy'])))
     f1.append((np.mean(scores['test_f1_macro']), np.std(scores['test_f1_macro'])))
     logger.info(f"\nAccuracy {model_name}:")
