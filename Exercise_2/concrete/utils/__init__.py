b7fcc5733780678726264079f5c6b519086778de
Lukas Sichert <lukas.sichert@gmail.com>
2024-11-26 14:55:44 +0100

    weiterversuchen

--- Exercise_2/concrete/utils/__init__.py
+++ Exercise_2/concrete/utils/__init__.py
@@ -1,5 +1,6 @@
 from .load_dataset import load_dataset
 from .config import Config
 from .logger import setup_logging
+from .load_dataset_notopcolumn import load_dataset_notopcolumn
 
-__all__ = ['Config', 'load_dataset', 'setup_logging']
+__all__ = ['Config', 'load_dataset', 'setup_logging','load_dataset_notopcolumn']
