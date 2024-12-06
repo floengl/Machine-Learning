e5d4d4a785363335bd57a98459ada7c11b671173
floengl <florian.engl@tuwien.ac.at>
2024-12-06 19:24:53 +0100

    first test run

---
+++ Exercise_2/concrete/random_forest.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ProcessPoolExecutor
+import numpy as np
+import pandas as pd
+from regression_tree import RegressionTree
+
+
+
+class ourRandomForestRegressor(object):
+    """
+    :param  nb_trees:       Number of decision trees to use
+    :param  nb_samples:     Number of samples to give to each tree
+    :param  max_depth:      Maximum depth of the trees
+    :param  max_workers:    Maximum number of processes to use for training
+    """
+    def __init__(self, nb_trees, nb_samples, max_depth=-1, max_workers=1, random_state=None):
+        self.trees = []
+        self.nb_trees = nb_trees
+        self.nb_samples = nb_samples
+        self.max_depth = max_depth
+        self.max_workers = max_workers
+        self.random_state = random_state
+        self.rng = np.random.default_rng(random_state)
+        #if random_state!=None:
+        #    random.seed(random_state)
+    """
+    Trains self.nb_trees number of decision trees.
+    :param  X:   Features
+    :param  y:   Target values
+    """
+    def fit(self, X, y):
+        if type(X) == np.ndarray:
+            data = list(zip(X, y))
+        else:
+            X= X.values
+            y = y.values
+            data = list(zip(X, y))
+        
+
+        with ProcessPoolExecutor(max_workers=self.max_workers) as executor:
+            indices = np.arange(len(data))
+            #chooses random indices to bootstrap the data
+            rand_ind = [self.rng.choice(indices, size=min(self.nb_samples, len(data)), replace=False) for _ in range(self.nb_trees)]
+            #extracts the data for the chosen indices
+            bootstrap_data = [[data[i] for i in ind] for ind in rand_ind]
+            #initializes randomly generated states for each tree   
+            random_states = self.rng.integers(low=0, high=1e6, size=self.nb_trees)
+            #builds the trees
+            self.trees = list(executor.map(self.train_tree, bootstrap_data, random_states))
+
+
+
+    """
+    Trains a single tree and returns it.
+    :param  data:   A List containing the index of the tree being trained
+                    and the data to train it
+    """
+    def train_tree(self, data, random_state):
+        if self.max_depth == -1:
+            tree = RegressionTree(random_state=random_state)
+        else:
+            tree = RegressionTree(max_depth=self.max_depth, random_state=random_state)
+            
+        X,y = zip(*data)
+        X = np.array(X)
+        y = np.array(y)
+        tree.fit(X, y)
+        return tree
+
+    """
+    Returns a prediction for the given feature. The result is the average of
+    the predictions from all trees.
+    :param  feature:    The features used to predict
+    """
+    def predict(self, feature):
+        if isinstance(feature, pd.DataFrame):
+           feature = feature.values
+           print(feature.shape)
+        predictions = np.array([tree.predict(feature) for tree in self.trees])
+        return np.mean(predictions, axis=0)
+
+
+
+
+
---
+++ Exercise_2/concrete/test_rf.py
@@ -0,0 +1,64 @@
+from utils import load_dataset, setup_logging
+from utils import logger
+from random_forest import ourRandomForestRegressor
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_validate, RepeatedKFold
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import Pipeline
+
+if __name__ == "__main__":
+
+    logger = setup_logging("test_random_forest")
+    X, y = load_dataset()
+   # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
+
+    scalers = [
+        ("RobustScaler", RobustScaler()),
+        ("None", Pipeline([("none", "passthrough")]))
+    ]
+
+    nb_trees=40
+    nb_samples=400
+    max_workers=12
+
+    logger.info(f"\nnb_trees={nb_trees},nb_samples={nb_samples}")
+    # models to test with
+    models = [
+        ("ourRFR", ourRandomForestRegressor(nb_trees=nb_trees, nb_samples=nb_samples, max_workers=max_workers, random_state=1234)),
+        ("scikit_RFR", RandomForestRegressor(random_state=1234)),
+        ("scikit_DTR", DecisionTreeRegressor(random_state=1234))
+    ]
+
+    for model_name, model in models:
+        mse = []
+        rmse = []
+        for name, scaler in scalers:
+
+            pipeline = Pipeline([
+                ("preprocessor", scaler),
+                ("model", model)
+            ])
+
+            cv=RepeatedKFold(n_splits=4, n_repeats=3, random_state=1234)
+
+            # Perform cross-validation
+            cv_results = cross_validate(pipeline, X, y, cv=cv, scoring=[ "neg_mean_squared_error"], return_train_score=True, n_jobs=-1)
+            
+            mse.append((name, -np.mean(cv_results['test_neg_mean_squared_error']), np.std(cv_results['test_neg_mean_squared_error'])))
+            root_mean_squared_error = np.sqrt(-np.mean(cv_results['test_neg_mean_squared_error']))
+            rmse.append((name, root_mean_squared_error))
+
+            logger.info(f"\nMSE {model_name}:")
+            logger.info(pd.DataFrame(mse, columns=['preprocessor', 'mean', 'std'])
+                        .sort_values(by='mean', ascending=True))
+            logger.info(f"\nRMSE {model_name}:")
+            logger.info(pd.DataFrame(rmse, columns=['preprocessor', 'rmean'])
+                        .sort_values(by='rmean', ascending=True))
+            
+
+
+
+   
