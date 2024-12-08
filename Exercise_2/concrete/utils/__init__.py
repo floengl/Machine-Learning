7916b2d423386b8debea4fae1182b2c890b8b8c4
floengl <florian.engl@tuwien.ac.at>
2024-12-08 10:34:06 +0100

    tried to run some tunes fixed some shit

--- Exercise_2/concrete/utils/__init__.py
+++ Exercise_2/concrete/utils/__init__.py
@@ -2,5 +2,6 @@ from .load_dataset import load_dataset
 from .config import Config
 from .logger import setup_logging
 from .load_dataset_notopcolumn import load_dataset_notopcolumn
+from .performance import relative_squared_error, rse_scorer
 
-__all__ = ['Config', 'load_dataset', 'setup_logging','load_dataset_notopcolumn']
+__all__ = ['Config', 'load_dataset', 'setup_logging','load_dataset_notopcolumn', 'relative_squared_error', 'rse_scorer']
