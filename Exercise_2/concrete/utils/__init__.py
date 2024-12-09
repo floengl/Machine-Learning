6f67ce71a2845a7ad19eabe19470de0beeabe36a
Florian Engl <Florian.Engl@tuwien.ac.at>
2024-12-09 13:30:56 +0100

    added modelcomparison files and plots

--- Exercise_2/concrete/utils/__init__.py
+++ Exercise_2/concrete/utils/__init__.py
@@ -2,6 +2,6 @@ from .load_dataset import load_dataset
 from .config import Config
 from .logger import setup_logging
 from .load_dataset_notopcolumn import load_dataset_notopcolumn
-from .performance import relative_squared_error, rse_scorer
+from .performance import relative_squared_error, rse_scorer, mse_scorer
 
-__all__ = ['Config', 'load_dataset', 'setup_logging','load_dataset_notopcolumn', 'relative_squared_error', 'rse_scorer']
+__all__ = ['Config', 'load_dataset', 'setup_logging','load_dataset_notopcolumn', 'relative_squared_error', 'rse_scorer', 'mse_scorer']
