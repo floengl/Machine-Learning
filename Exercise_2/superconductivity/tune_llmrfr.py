e2a57f68ff3643f601e633224bc7b3b61fb1b0bf
Florian Engl <Florian.Engl@tuwien.ac.at>
2024-12-10 14:08:24 +0100

    changed tune llm

--- Exercise_2/superconductivity/tune_llmrfr.py
+++ Exercise_2/superconductivity/tune_llmrfr.py
@@ -37,7 +37,7 @@ def main():
         n_jobs=-1,
         verbose=1,
         random_state=1234,
-        n_iter=50
+        n_iter=25
     )
 
     # fit
@@ -53,28 +53,28 @@ def main():
     logger.info("\n")
 
     # optimizer
-    optimizer = BayesSearchCV(
-        estimator=estimator,
-        search_spaces=search_space,
-        scoring="neg_mean_squared_error",
-        cv=cv,
-        n_jobs=-1,
-        verbose=1,
-        random_state=1234,
-        n_iter=50
-    )
+    #optimizer = BayesSearchCV(
+     #   estimator=estimator,
+      #  search_spaces=search_space,
+       # scoring="neg_mean_squared_error",
+        #cv=cv,
+        #n_jobs=-1,
+        #verbose=1,
+        #random_state=1234,
+        #n_iter=50
+    #)
 
     # fit
-    optimizer.fit(X, y)
+    #optimizer.fit(X, y)
 
     # log best score and parameters
-    logger.info(f"Scoring: MSE")
-    logger.info(f"Best score: {optimizer.best_score_}")
-    logger.info(f"best params: {optimizer.best_params_}")
-    df = pd.DataFrame(optimizer.cv_results_)[["params", "mean_test_score", "std_test_score", "rank_test_score"]]
-    logger.info("CV results:")
-    logger.info(df.sort_values("rank_test_score").to_string())
-    logger.info("\n")
+    #logger.info(f"Scoring: MSE")
+    #logger.info(f"Best score: {optimizer.best_score_}")
+    #logger.info(f"best params: {optimizer.best_params_}")
+    #df = pd.DataFrame(optimizer.cv_results_)[["params", "mean_test_score", "std_test_score", "rank_test_score"]]
+    #logger.info("CV results:")
+    #logger.info(df.sort_values("rank_test_score").to_string())
+    #logger.info("\n")
 
 if __name__ == "__main__":
     main()
