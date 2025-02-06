9a0c86db64250528cc37e67e2f2c9c99a6e77b63
floengl <florian.engl@tuwien.ac.at>
2025-02-06 15:48:15 +0100

    forgot something

--- Exercise_3/wine/utils/__init__.py
+++ Exercise_3/wine/utils/__init__.py
@@ -5,4 +5,4 @@ from .logger import setup_logging
 from .performance import relative_squared_error, rse_scorer, mse_scorer
 
 
-__all__ = ['Config', 'load_dataset_red', 'setup_logging', 'relative_squared_error', 'rse_scorer', 'mse_scorer']
+__all__ = ['Config', 'load_dataset_red', 'load_dataset_white', 'setup_logging', 'relative_squared_error', 'rse_scorer', 'mse_scorer']
