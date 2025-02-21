c887f48632947fdb01f8eee859877aecc8cbf5dc
floengl <florian.engl@tuwien.ac.at>
2025-02-21 12:16:04 +0100

    addded search everywhere couldnt run one bc of time issues

---
+++ Exercise_3/superconductivity/search.py
@@ -0,0 +1,91 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.svm import LinearSVR
+from sklearn.neighbors import KNeighborsRegressor
+from utils import load_dataset, setup_logging
+from utils import logger
+import os
+from utils.config import Config
+from datetime import datetime
+
+from simulated_annealing_3 import *
+
+logger = setup_logging("search")
+
+random_seed = 42
+alpha = 0.85
+maxtime = 120
+mintime = 60
+f = 5
+n_repeats = 5
+T_0 = 400
+update_iters = 3
+go_to_best_multiple = 4
+
+models = {"rf": RandomForestRegressor,
+          "LinearSVR": LinearSVR,
+          "Ridge": Ridge,
+          "Lasso": Lasso,
+          "KNN": KNeighborsRegressor}
+
+
+start_params = {"rf": {"n_estimators": 100, "max_depth": 10, "max_features": "log2"},
+                "LinearSVR": {"max_iter": 1000, "C": 1.0, "fit_intercept": True},
+                "Ridge": {"alpha": 1.0, "fit_intercept": True},
+                "Lasso": {"alpha": 1.0, "fit_intercept": True},
+                "KNN": {"n_neighbors": 5, "p": 5}}
+
+params_vals = {"rf": {"n_estimators": [10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,500,600,700,800,900], "max_depth": [5,10,20,30,50,75,100,150,200],
+                      "max_features": ["sqrt", "log2", None]},
+                "LinearSVR": {"max_iter": [1000,2000,5000,10000], "C": [10**i for i in range(-3, 4)], "fit_intercept": [True, False]},
+                "Ridge": {"alpha": [10**i for i in range(-3, 4)], "fit_intercept": [True, False]},
+                "Lasso": {"alpha": [10**i for i in range(-3, 4)], "fit_intercept": [True, False]},
+                "KNN": {"n_neighbors": [3,5,10,20], "p": [1,2,3,4,5]}}
+
+X, Y = load_dataset()
+
+
+model, results, nr_reheats, extime, T, best_i, best_time = simulate_annealing(start_params, params_vals, X, Y, models =  models, train_model=train_model_2, maxiters=10000000, T_0=T_0, f=f, n_repeats=n_repeats, random_seed=random_seed, alpha=alpha, mintime=mintime, maxtime=maxtime, update_iters=update_iters, go_to_best_multiple=go_to_best_multiple)
+
+# Use the RESULTS_DIR from the config file
+results_dir = Config.RESULTS_DIR
+os.makedirs(results_dir, exist_ok=True)
+
+# Get the current timestamp and format it
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Create the results filename with the timestamp
+results_file = os.path.join(results_dir, f"results_{timestamp}.csv")
+results.to_csv(results_file, index=False)
+
+
+logger.info(f"Random Seed: {random_seed}")
+logger.info(f"folds: {f}, repeats: {n_repeats}")
+logger.info(f"Alpha: {alpha}")
+logger.info(f"Max time: {maxtime}") 
+logger.info(f"Min time: {mintime}")
+logger.info(f"Decrease temperature after {update_iters} iterations")
+logger.info(f"Go to best model after {go_to_best_multiple*update_iters} iterations")
+
+logger.info(F"Inital temperature: {T_0}\n\nFindings:\n")
+logger.info(f"Best model: {model}")
+logger.info(f"Best score: {results['Best Metric'].min()}")
+logger.info(f"Number of reheats: {nr_reheats}")
+logger.info(f"Execution time: {extime/60} minutes")
+logger.info(f"Final temperature: {T}")
+logger.info(f"Best iteration: {best_i}")
+logger.info(f"Best results achieved after {best_time} minutes")
+logger.info(f"Results saved to: {results_file}")
+
+
+
+# Log the entire DataFrame
+logger.info(f"History:\n{results}\n\n")
+logger.info(f"Parameter values: {params_vals}")
+
+print(model)
+print(results)
+
+
+
+
---
+++ Exercise_3/wine/simulated_annealing_3.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import cross_val_score, RepeatedKFold
+import copy
+import time
+
+
+
+
+# Sample train_model function #
+###############################
+
+def train_model_1(models, curr_model, curr_params, Xtrain, Xvalid, Ytrain, Yvalid):
+
+     model = models[curr_model](**curr_params)
+     model.fit(Xtrain, Ytrain)
+     preds = model.predict(Xvalid)
+     metric_val = root_mean_squared_error(Yvalid, preds) # Any metric can be used
+
+     return model, metric_val
+
+
+def train_model_2(models, curr_model, curr_params, X, Y, f=5, n_repeats=1, random_seed=42):
+    np.random.seed(random_seed)
+    model = models[curr_model](**curr_params)
+    rkf = RepeatedKFold(n_splits=f, n_repeats=n_repeats, random_state=random_seed)
+    cv_scores = cross_val_score(model, X, Y, cv=rkf, scoring='neg_root_mean_squared_error')
+    mean_cv_score = -cv_scores.mean()  # Convert negative RMSE to positive RMSE
+
+    return model, mean_cv_score
+
+
+def choose_params(curr_model, params_vals, curr_params=None, T=400, T_0=400, random_seed=42):
+    np.random.seed(random_seed)
+    if curr_params[curr_model] is not None:
+        next_params = copy.deepcopy(curr_params[curr_model])
+        param_to_update = np.random.choice(list(params_vals[curr_model].keys()))
+        curr_index = params_vals[curr_model][param_to_update].index(curr_params[curr_model][param_to_update])
+
+        # Determine the new index using a normal distribution around the current index
+        std_dev = max(1, int(len(params_vals[curr_model][param_to_update]) * T / (2 * T_0)))
+        new_index = int(np.random.normal(loc=curr_index, scale=std_dev))
+        new_index = max(0, min(len(params_vals[curr_model][param_to_update]) - 1, new_index))  # Ensure index is within bounds
+
+        next_params[param_to_update] = params_vals[curr_model][param_to_update][new_index]
+    else:
+        next_params = {k: np.random.choice(v) for k, v in params_vals[curr_model].items()}
+
+    return next_params
+
+
+def choose_model(models, best_model=None, prev_model=None, T=400, T_0=400, go_to_best_model=False, random_seed=42):
+  
+    np.random.seed(random_seed)
+    if go_to_best_model:
+        return best_model
+    non_switch_probability = 1 - np.exp(-0.25 * (T_0 / T) ** 2)  # starts with a higher switch probability and decreases faster than acceptance probability for worse solutions due to squaring the value
+    if np.random.rand() < non_switch_probability and prev_model is not None:
+        return prev_model
+    else:
+        return np.random.choice(list(models.keys()))
+    
+
+
+def simulate_annealing(start_params, param_vals, X, Y,  models, train_model=train_model_2, maxiters=100, alpha=0.95, beta=1.3, T_0=400, update_iters=5, f=5, n_repeats=1, random_seed=42, mintime=60, maxtime=120, go_to_best_multiple=5):
+
+    # Start the timer
+    start_time = time.time()
+
+    rng = np.random.RandomState(random_seed)
+    columns = ['Model'] + [f"{model}_{param}" for model in models for param in start_params[model].keys()] + ['Metric', 'Best Metric']
+    results = pd.DataFrame(index=range(maxiters), columns=columns)
+    best_metric = float('inf')
+    prev_metric = float('inf')
+    prev_params = copy.deepcopy(start_params)
+    best_params = copy.deepcopy(start_params)
+    prev_model = None
+    best_model = None
+    counter_1 = 0
+    counter_2 = 0
+    best_i = 0
+    best_time = 0
+
+    go_to_best_model= False
+    T = T_0
+
+    for i in range(maxiters):
+        print('Starting Iteration {}'.format(i))
+
+        curr_seed = rng.randint(0, 1000000)
+        curr_model = choose_model(models, best_model, prev_model, T, T_0, go_to_best_model, random_seed=curr_seed)
+        curr_seed = rng.randint(0, 1000000)
+        curr_params = choose_params(curr_model, param_vals, prev_params, T, T_0, random_seed=curr_seed)
+        print('Model: {} Parameters: {}'.format(curr_model, curr_params))
+
+        curr_seed = rng.randint(0, 1000000)
+        model, metric = train_model(models, curr_model, curr_params, X, Y, f=f, n_repeats=n_repeats, random_seed=curr_seed)
+
+        if metric < prev_metric:
+            print('Local Improvement in metric from {:8.4f} to {:8.4f} '
+                  .format(prev_metric, metric) + ' - parameters accepted')
+            prev_model = curr_model
+            prev_params[curr_model] = copy.deepcopy(curr_params)
+            prev_metric = metric
+
+            if metric < best_metric:
+                print('Global improvement in metric from {:8.4f} to {:8.4f} '
+                      .format(best_metric, metric) +
+                      ' - best parameters updated')
+                best_model = curr_model
+                best_metric = metric
+                best_params[curr_model] = copy.deepcopy(curr_params)
+                best_model_final = model
+                best_i = i
+                best_time = (time.time() - start_time)/60
+
+        else:
+            rnd = rng.uniform()
+            diff = metric - prev_metric
+            threshold = np.exp(-beta * diff / T)
+            if rnd < threshold:
+                print('No Improvement but parameters accepted. Metric change' +
+                      ': {:8.4f} threshold: {:6.4f} random number: {:6.4f}'
+                      .format(diff, threshold, rnd))
+                prev_metric = metric
+                prev_params[curr_model] = copy.deepcopy(curr_params)
+            else:
+                print('No Improvement and parameters rejected. Metric change' +
+                      ': {:8.4f} threshold: {:6.4f} random number: {:6.4f}'
+                      .format(diff, threshold, rnd))
+                counter_1 += 1
+
+         # Store the model name
+        results.loc[i, 'Model'] = curr_model
+
+        # Store the hyperparameters for the current model
+        for param, value in curr_params.items():
+            results.loc[i, f"{curr_model}_{param}"] = value
+
+        # Store the metrics
+        results.loc[i, 'Metric'] = metric
+        results.loc[i, 'Best Metric'] = best_metric
+
+        go_to_best_model = False
+        if i % update_iters == 0:
+            T = alpha * T
+            print('Temperature: {}'.
+                  format(T))
+        if i % update_iters*go_to_best_multiple == 0:
+            go_to_best_model = True
+        if counter_1 == 5 or T<0.01:
+            print('stagnant')
+            print('Temperature: {}'.
+                  format(T))
+            if time.time() - start_time > mintime*60:
+                break
+            else:
+                counter_1 = 0
+                counter_2 += 1   
+                T = T_0*(mintime*60-(time.time()-start_time))/(mintime*60) # Reheats to temperature that declines with time
+                print('Reheat')
+        
+
+        if time.time() - start_time > 60*maxtime:
+            break
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {end_time - start_time} seconds")
+    return  best_model_final, results, counter_2, execution_time, T, best_i, best_time
